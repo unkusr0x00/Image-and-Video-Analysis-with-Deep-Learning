07447f07f5411ed3808eeb7fdc1e23282d49f3be
Aron Wedam <arwedam@edu.aau.at>
2024-05-31 18:33:26 +0200

    ADD: Search bar

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,6 +4,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchComponent } from './search/search.component';
+import { FormsModule } from "@angular/forms";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from "@angular/material/input";
+import { MatIconModule } from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
 
 const routes: Routes = [
   { path: 'search', component: SearchComponent },
@@ -18,7 +23,12 @@ const routes: Routes = [
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/search/search.component.ts
+++ frontend/src/app/search/search.component.ts
@@ -7,9 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
+  query: string = '';
+  videos = [    //TODO: Placeholder data
+    {
+      id: '1212451',
+      thumbnails: [
+        { src: 'assets/Placeholder.png', time: '12:32:12' },
+        { src: 'assets/Placeholder.png', time: '12:32:15' },
+        { src: 'assets/Placeholder.png', time: '12:32:18' },
+        { src: 'assets/Placeholder.png', time: '12:32:21' },
+        { src: 'assets/Placeholder.png', time: '12:32:24' }
+      ]
+    },
+    {
+      id: '1235413',
+      thumbnails: [
+        { src: 'assets/Placeholder.png', time: '00:03:14' },
+        { src: 'assets/Placeholder.png', time: '00:03:21' }
+      ]
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+
+
+  search() {
+    // Hier wird die Logik zur Verarbeitung der Suchanfrage eingefügt.
+    console.log(this.query);
+  }
 }
